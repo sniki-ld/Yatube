fb3cf047fc0d7f060ef76c7741b32aa469d4c445
sniki-ld <dennisalenaf@gmail.com>
2021-11-24 14:44:20 +0300

    Добавлены исправления после 1 ревью 6 спринта

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.db import IntegrityError
 from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import CommentForm, PostForm
